b4a080c6dd0b969b8da8626164f79eda2841474b
john <johnmaluki58@gmail.com>
2023-09-30 17:10:04 +0300

    Create table models

--- app/models.py
+++ app/models.py
@@ -2,9 +2,31 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Hero(db.Model):
-    __tablename__ = 'hero'
+    __tablename__ = "heroes"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-# add any models you may need. 
+
+class Power(db.Model):
+    __tablename__ = "powers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+class HeroPower(db.Model):
+    __tablename__ = "hero_powers"
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String)
+    hero_id = db.Column(db.Integer, db.ForeignKey("heroes.id"))
+    power_id = db.Column(db.Integer, db.ForeignKey("powers.id"))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
